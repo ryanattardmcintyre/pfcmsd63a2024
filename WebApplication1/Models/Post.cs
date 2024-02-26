8c4f3bfab896448b8249d33b3b0d6aa032dc4266
Ryan Attard <ryanattard@gmail.com>
2024-02-26 09:46:32 +0100

    Started Firestore Part 1

---
+++ WebApplication1/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using Google.Cloud.Firestore;
+
+namespace WebApplication1.Models
+{
+    [FirestoreData]
+    public class Post
+    {
+      
+        public string Id { get; set; }
+        
+        [FirestoreProperty]
+        public string Title { get; set; }
+
+        [FirestoreProperty]
+        public string Content { get; set; }
+
+        [FirestoreProperty]
+        public Timestamp DateCreated { get; set; }
+        [FirestoreProperty]
+        public Timestamp DateUpdated { get; set; }
+
+        [FirestoreProperty]
+        public string Photo { get; set; }
+
+        public string BlogId { get; set; }
+    }
+}
---
+++ WebApplication1/Repositories/BlogsRepository.cs
@@ -0,0 +1,37 @@
+﻿using Google.Cloud.Firestore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repositories
+{
+
+    //The repository classes will manage the data around (fetching data from the database, writing data to the
+    //database, deleting data from the database, updating data to the database)
+
+    //Implement CRUD operations
+    //Create, Read, Update, Delete
+    public class BlogsRepository
+    {
+
+
+        public BlogsRepository() {
+          //dependency injection
+          //
+          //FirestoreDb db = FirestoreDb.Create(project);
+
+        }
+
+
+        /// <summary>
+        /// This method will insert a new blog instance in the No-Sql database 
+        /// </summary>
+        /// <param name="b"></param>
+        public async void AddBlog(Blog b)
+        {
+            DocumentReference docRef = db.Collection("blogs").Document(b.Id);
+            await docRef.SetAsync(b);
+
+        }
+
+
+    }
+}
