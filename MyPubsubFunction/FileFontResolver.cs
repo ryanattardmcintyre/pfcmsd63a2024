f0f710db9251634de62e31cd613906c5fbe97fd2
Ryan Attard <ryanattard@gmail.com>
2024-04-10 10:26:29 +0200

    PubSub function complete

--- MyPubsubFunction/FileFontResolver.cs
+++ MyPubsubFunction/FileFontResolver.cs
@@ -3,6 +3,7 @@ using PdfSharp.Fonts;
 using PdfSharp.Pdf;
 using System;
 using System.IO;
+using Google.Cloud.Storage.V1;
  
  namespace MyPubsubFunction{
  public class FileFontResolver : IFontResolver // FontResolverBase
@@ -11,7 +12,9 @@ using System.IO;
 
         public byte[] GetFont(string faceName)
         {
-            using (var ms = new MemoryStream())
+            //this snippet of code is going to open a file called Verdana.ttf from a folder
+            //called bin which will be generated at runtime thus erasing whatever is already inside it
+           /* using (var ms = new MemoryStream())
             {
                 using (var fs = File.Open(faceName, FileMode.Open))
                 {
@@ -19,28 +22,40 @@ using System.IO;
                     ms.Position = 0;
                     return ms.ToArray();
                 }
-            }
+            }*/
+
+            //solution: we can place the Verdana.ttf in a bucket
+            //          we can read the Verdana.ttf from the online bucket
+            //https://storage.cloud.google.com/msd63apfcra_fg/Verdana.ttf
+
+                var storage = StorageClient.Create();
+                MemoryStream stream = new MemoryStream();
+                storage.DownloadObject("msd63apfcra_fg", "Verdana.ttf", stream);
+                stream.Position=0;
+                return stream.ToArray();
+
         }
 
+ 
         public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
         {
             if (familyName.Equals("Verdana", StringComparison.CurrentCultureIgnoreCase))
             {
                 if (isBold && isItalic)
                 {
-                    return new FontResolverInfo("Fonts/Verdana-BoldItalic.ttf");
+                    return new FontResolverInfo("Verdana-BoldItalic.ttf");
                 }
                 else if (isBold)
                 {
-                    return new FontResolverInfo("Fonts/Verdana-Bold.ttf");
+                    return new FontResolverInfo("Verdana-Bold.ttf");
                 }
                 else if (isItalic)
                 {
-                    return new FontResolverInfo("Fonts/Verdana-Italic.ttf");
+                    return new FontResolverInfo("Verdana-Italic.ttf");
                 }
                 else
                 {
-                    return new FontResolverInfo("Fonts/Verdana.ttf");
+                    return new FontResolverInfo("Verdana.ttf");
                 }
             }
             return null;
